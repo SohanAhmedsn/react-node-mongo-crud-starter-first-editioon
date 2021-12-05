d3a0a19d3dea4d8b2b1afa2ad1df944dd3810c76
sn77sohan <sohan.ahmedself@gmail.com>
2021-12-05 21:38:14 +0600

    react-node-mongo-crud-starter-first-edition

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2>This is Update User</h2>
-        </div>
-    );
+  const [user, setUser] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const url = `http://localhost:3000/users/update/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+  const handleUpdateUser = (e) => {
+
+  };
+  return (
+    <div>
+      <h2>Name: {user.name}</h2>
+      <p>
+        <small>ID: {id}</small>{" "}
+      </p>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" name="" id="" />
+        <input type="email" name="" id="" />
+        <input type="submit" value="Subdate" />
+      </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
